3246858ed7d87d68a974d8d0dc59a13c066ba7ba
axelvestberg <axel.vestberg@gmail.com>
2019-03-27 18:33:23 +0100

    fixed eslint and prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Friends from './components/friends';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Friends from "./components/friends";
 
 class App extends Component {
   render() {
     return (
-
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-
-
-          
-                    <h1 className="App-title">React + Nodejs Boilerplate</h1>
+          <h1 className="App-title">React + Nodejs Boilerplate</h1>
         </header>
         <Friends />
       </div>
