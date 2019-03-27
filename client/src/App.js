4c22f8050352827905099b28eeb7a0f8b9600380
axelvestberg <axel.vestberg@gmail.com>
2019-03-27 18:22:38 +0100

    test commit

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React + Nodejs Boilerplate</h1>
+
+
+          
+                    <h1 className="App-title">React + Nodejs Boilerplate</h1>
         </header>
         <Friends />
       </div>
