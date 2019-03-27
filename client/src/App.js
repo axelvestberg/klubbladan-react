6ca397c07e4055f788dce2a68c08d3d32b577e58
axelvestberg <axel.vestberg@gmail.com>
2019-03-27 18:21:33 +0100

    testing pretty-quick format on commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import Friends from './components/friends';
 
-
-let x;
-
 class App extends Component {
   render() {
     return (
+
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
